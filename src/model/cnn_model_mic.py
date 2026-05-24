c845b8783a91f029d08db2261b5bdbf5f63fb63b
Jan Kavcl <jankavcl900@gmail.com>
2026-05-24 23:05:04 +0200

    SCRUM-105 fixano pri __init__(), forward

--- src/model/cnn_model_mic.py
+++ src/model/cnn_model_mic.py
@@ -4,11 +4,14 @@ import torch.nn as nn
 
 class Branch(nn.Module):
 
+    # v vhod pride tak (batch, 1, 129, 62)
+    # 62 je cas, 129 je freq_bins, 1 ker je mono, batch -> stevilo slik
     def __init__(self):
         super().__init__()
 
         self.network = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            # zaradi velikega dinamičnega razpona je treba normalizirat
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2),
@@ -19,3 +22,6 @@ class Branch(nn.Module):
             nn.AdaptiveAvgPool2d((4, 4)),
             nn.Flatten(),  # (512), ker je en
         )
+
+    def forward(self, x):
+        return self.network(x)
