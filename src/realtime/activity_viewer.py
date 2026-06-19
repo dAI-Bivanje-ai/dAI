d50ccb61e0a9f251b5677ca5a3a5186cdfa61c7c
Jan Kavcl <jankavcl900@gmail.com>
2026-06-19 13:51:46 +0200

    SCRUM-193 implementiran osnovni activity_viewer, ki izpiše aktivno okno

--- src/realtime/activity_viewer.py
+++ src/realtime/activity_viewer.py
@@ -1,5 +1,5 @@
 import pywinctl
-
+import time
 
 def get_active_window():
     app_name = None
@@ -21,6 +21,54 @@ def get_active_window():
     return app_name, title
 
 
+PRODUKTIVNE = {
+    "kitty",
+    "ghostty",
+    "pycharm",
+    "PyCharm",
+    "Code",
+}
+
+NEPRODUKTIVNE = {
+    "Discord",
+    "firefox",
+    "Firefox",
+    "netflix",
+    "Spotify",
+}
+
+
+class ActivityViewer:
+
+    def __init__(self):
+        self.last_active_app_name = None
+        self.last_active_title = None
+        self.interval = 1
+
+    def categorize(self, app_name):
+        if app_name in PRODUKTIVNE:
+            return 0
+        if app_name in NEPRODUKTIVNE:
+            return 1
+        return None
+
+    def run(self):
+
+        while True:
+            app_name, title = get_active_window()
+
+            if app_name != self.last_active_app_name:
+                category = self.categorize(app_name)
+                label = {0: "PRODUKTIVNE", 1: "NEPRODUKTIVNE"}.get(category, "NEZNANO")
+                print(f"{app_name} -> {label}")
+
+                self.last_active_app_name = app_name
+                self.last_active_title = title
+
+            time.sleep(self.interval)
+
+
+
+
 if __name__ == "__main__":
-    app_name, title = get_active_window()
-    print(app_name, title)
+    ActivityViewer().run()
